7c1627b6d972c9ab1773b7b16470cc62c708daaf
Vladimir <vladimir-denisov87@yandex.ru>
2023-09-12 19:52:15 +0300

    add Swagger, mapping change

--- src/main/java/main/controller/UserController.java
+++ src/main/java/main/controller/UserController.java
@@ -1,5 +1,6 @@
 package main.controller;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import main.models.User;
 import main.models.UserPostDto;
@@ -14,35 +15,39 @@ import java.util.List;
 @RestController
 @RequestMapping("/user-api/")
 @RequiredArgsConstructor
+
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @ApiOperation("Получение всех пользователей")
     @GetMapping("all")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
-
+    @ApiOperation("Добавление пользователя")
     @PostMapping("add")
     public User addUser(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, @RequestParam String userName) {
         UserPostDto dto = new UserPostDto(firstName, lastName, email, userName);
         return userService.addUser(dto);
     }
 
+    @ApiOperation("Удаление всех пользователей")
     @DeleteMapping("deleteAll")
     public void deleteUsers() {
         userService.deleteAll();
     }
-
-    @DeleteMapping("delete")
-    public void deleteUser(@RequestParam Integer id) {
+    @ApiOperation("Удаление пользователя по его ID")
+    @DeleteMapping("delete/{id}")
+    public void deleteUser(@PathVariable Integer id) {
         userService.deleteById(id);
     }
 
-    @PostMapping("update")
-    public void updateUser(@RequestParam Integer id, @RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, @RequestParam String userName) {
+    @ApiOperation("Обновление информации пользователя по его ID")
+    @PostMapping("update/{id}")
+    public void updateUser(@PathVariable Integer id, @RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, @RequestParam String userName) {
         UserPostDto dto = new UserPostDto(firstName, lastName, email, userName);
         userService.updateUserById(id, dto);
     }
